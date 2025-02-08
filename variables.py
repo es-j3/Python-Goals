04f11a5542130cce37432067998a7bb1e663d0ba
es <160103673+es-j3@users.noreply.github.com>
2025-02-07 21:56:37 -0800

    github vscode extension sucks

---
+++ variables.py
@@ -0,0 +1,38 @@
+# Variable = A container for a value (String, Integer, Float, Boolean)
+#           A variable behaves as if it was the value it contains
+# Strings
+first_name = "es"
+food = "pizza"
+email = "coolemail@cooldomain.org"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+# Integers
+age = 14
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 4.0
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your GPA is {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean
+
+is_student = True
+for_sale = False
+is_online = True
+
+print(f"Are you a student?: {is_student}")
+print(f"Is this item for sale?: {for_sale}")
+print(f"Are you online?: {is_online}")
