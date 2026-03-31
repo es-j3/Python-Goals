716345c76682fc939f23b3e0d023099d0753d2cf
John Doe <esj3cc@gmail.com>
2026-03-30 21:41:15 -0700

    stuff

---
+++ 1.py
@@ -0,0 +1,3 @@
+# "This is my first python program..."
+print("Me gusta la pizza")
+print("Es muy deliciosa!")
---
+++ 2.py
@@ -0,0 +1,54 @@
+#strings
+first_name = 'John'
+food = 'Pierogi'
+email = "realemail@gmail.com"
+
+# print(f"Hello {first_name}")
+# printf(f"Te gusta {food}")
+# printf(f"Your email is {email}")
+
+#integers
+age = 67
+quantity = 56
+student_amount = 50
+
+# print(f"Masz {age} lat")
+# print(f"You are buying {quantity} items")
+# print(f"In your class, there are {student_amount} students")
+
+# float
+
+price = 99.99
+gpa = 1.0
+dystans = 6.6
+
+# print(f"The price is {price}zł")
+# print(f"Wow... your GPA is {gpa}, amazing work!")
+# print(f"You ran {dystans} kilos!")
+
+#bools
+
+is_sigma = False
+for_sale= True
+is_larping = True
+
+print(f"You sigma? {is_sigma}")
+
+if is_sigma:
+    print("A sigma!")
+else:
+    print("You a lie!")
+
+print("Is this for sale?")
+
+if for_sale:
+    print("For sale!")
+else:
+    print("Not for sale")
+
+print("Larper detected?")
+
+if is_larping:
+    print("Larper! Larper!")
+else:
+    print("Son.......")
