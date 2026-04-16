495e78cc58b5b72b4bdec453e408eac673b0bcda
Your Name <your.email@example.com>
2026-04-15 20:02:22 -0700

    updates

--- Languages/Python/13.py
+++ Languages/Python/13.py
@@ -1,7 +1,30 @@
-name = input("Enter your full name: ")
+#name = input("Enter your full name: ")
 
-result = len(name)
-result = name.find("x")
-result = name.rfind("l")
-name = name.capitalize()
-print(result)
+#result = len(name)
+#result = name.find("x")
+#result = name.rfind("l")
+#name = name.capitalize()
+#name = name.upper()
+#name = name.lower()
+#result = name.isdigit()
+#result = name.isalpha()
+
+#print(result)
+
+#phone_number = input("Enter phone #: ")
+
+#result = phone_number.count("-")
+#phone_number = phone_number.replace("-", "")
+
+#print(phone_number)
+
+username = input("Enter username: ")
+
+if len(username) > 12:
+    print("Your username cannot be longer than 12 characters!")
+elif not username.find(" ") == -1:
+    print("Username must not contain spaces!")
+elif not username.isalpha():
+    print("Username cannot have numbers!")
+else:
+    print(f"Welcome {username}!")
---
+++ Languages/Python/15.py
@@ -0,0 +1,7 @@
+price1 = 3000.14159
+price2 = -98700.65
+price3 = 12000.34
+
+print(f"Price 1's ${price1:+,.2f}")
+print(f"Price 2's ${price2:+,.2f}")
+print(f"Price 3's ${price3:+,.2f}")
